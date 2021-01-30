d244062f7869d78a517a6851da366386c956ebab
Pavel Sakun <axelhomkin@gmail.com>
2021-01-30 14:24:26 +0200

    finised timer, stiles default, any additional fiches ...

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
-  });
+const refs = {
+  dayInterface: document.querySelector('[data-value="days"]'),
+  hourInterface: document.querySelector('[data-value="hours"]'),
+  minInterface: document.querySelector('[data-value="mins"]'),
+  secInterface: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.intervalId = null;
+  }
+  startTimer() {
+    const timeBefore = this.targetDate;
+    upgradeInterface(0);
   
-  
-//   Для подсчета значений используй следующие готовые формулы, 
-//   где time - разница между targetDate и текущей датой.
-  
- 
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const leftTime = timeBefore - currentTime;
+      upgradeInterface(leftTime);
+    }, 1000);
+  }
+  stopTimer(){
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    upgradeInterface(0);
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Feb 11, 2021"),
+});
+
+// timer.startTimer();
+// timer.stopTimer();
+
+function upgradeInterface(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  refs.dayInterface.textContent = `${days}`;
+  refs.hourInterface.textContent = `${hours}`;
+  refs.minInterface.textContent = `${mins}`;
+  refs.secInterface.textContent = `${secs}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
